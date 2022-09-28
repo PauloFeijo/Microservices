ea74d8fb1d7a66f52d16732b78d29947d4d3c9b8
PauloFeijo <paulorikrdo.89@gmail.com>
2022-09-28 09:29:51 -0300

    Remove SqlServer and add mongo on infrastructure

--- Undeploy.bat
+++ Undeploy.bat
@@ -2,3 +2,4 @@ docker-compose -f Microservice.Query.Api\docker-compose.yml -f Microservice.Quer
 docker-compose -f Microservice.Producer.Api\docker-compose.yml -f Microservice.Producer.Api\docker-compose.override.yml down
 docker-compose -f Microservice.Consumer.Worker\docker-compose.yml -f Microservice.Consumer.Worker\docker-compose.override.yml down
 docker-compose -f Infrastructure\docker-compose.yml down
+pause
